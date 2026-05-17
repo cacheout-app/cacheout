a49f3cbaa4a48cfcefbd9d74139cc34e03b46001
Bonanza <dan@dan.ac>
2026-05-17 08:52:54 -0700

    chore: update Homebrew cask to v2.1.0
    sha256 of build/Cacheout-2.1.0.dmg after notarization + stapling.

--- homebrew/cacheout.rb
+++ homebrew/cacheout.rb
@@ -1,8 +1,8 @@
 cask "cacheout" do
   # NOTE: version and sha256 are filled at release time after notarized DMG is produced.
   # Do NOT `brew install` from this file until a release artifact exists.
-  version "2.0.0"
-  sha256 "6de067232a27cdf2d46c20b6b8d5d135b8d2cb21199f13b9fe384ae7abb66942"
+  version "2.1.0"
+  sha256 "55291e6e562dda4ee4ecb786aca97e01fb4b0f5c5d44500365a6aab69ec0d889"
 
   url "https://github.com/cacheout-app/cacheout/releases/download/v#{version}/Cacheout-#{version}.dmg"
   name "Cacheout"
